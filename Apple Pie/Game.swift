5ce3866a24a2a7e27941dbaedb11db39bfa1b5c5
ART_List <artbdarm9@mail.ru>
2020-12-19 22:19:44 +0300

    added struct

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,14 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Artem Listopadov on 12/19/20.
+//  Copyright © 2020 Artem Listopadov. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+var listOfWords = ["buccaneer","swift","glorious","incandescent","bug", "program"]
+let incorrectMovesAllowed = 7
+var totalWins = 0
+var totalLosses = 0
+
+
 class ViewController: UIViewController {
     @IBOutlet weak var treeImageView: UIImageView!
     @IBOutlet weak var correctWordLabel: UILabel!
@@ -17,9 +23,22 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        newRound()
     }
-
+    
+    var currentGame: Game!
+    
+    func newRound() {
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining:
+        incorrectMovesAllowed)
+        updateUI()
+    }
+    func updateUI() {
+        scoreLabel.text = "Wins:\(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+    }
+    
     @IBAction func letterButtonPressed(_ sender: UIButton) {
         sender.isEnabled = false
     }
